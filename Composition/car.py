06734c1c4a9342941cc6cc3cfac5c26b1f09a6f0
moulidmahat <159256689+moulidmahat@users.noreply.github.com>
2024-11-06 14:45:13 +0300

    Update car.py

---
+++ Composition/car.py
@@ -0,0 +1,12 @@
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def display_info(self):
+        print(f"Car Make: {self.make}, Model: {self.model}, Year: {self.year}")
+
+# Example usage
+car = Car("Toyota", "Camry", 2020)
+car.display_info()
